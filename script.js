6c5d2c5d030d6156d1aa70f2bb9f6e5f23727bfc
GustavoSalamy <iphonegustavosalamy@outlook.com>
2023-12-01 00:09:13 -0300

    adit efeito

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const myobserver = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
-        if (entry.isIntersecting){
+        if (entry.isIntersecting) {
             entry.target.classList.add('dicasanimation') + entry.target.classList.remove('dicasinternas')
         } else {
             entry.target.classList.remove('dicasanimation')
@@ -10,5 +10,34 @@ const myobserver = new IntersectionObserver((entries) => {
 
 const elements = document.querySelectorAll('#dicas')
 
-elements.forEach((element)=> myobserver.observe(element))
+elements.forEach((element) => myobserver.observe(element))
 
+
+
+const myobserverh2 = new IntersectionObserver((h2element) => {
+    h2element.forEach((h2entry) => {
+        if (h2entry.isIntersecting) {
+            h2entry.target.classList.add('quemsoueuanimate') + h2entry.target.classList.remove('quemsoueu')
+        } else {
+            h2entry.target.classList.remove('quemsoueuanimate')
+        }
+    })
+})
+
+const elementh2 = document.querySelectorAll('.quemsoueu')
+elementh2.forEach((elementsh2) => myobserverh2.observe(elementsh2))
+
+
+
+const myobservertopo = new IntersectionObserver((topoelement) => {
+    topoelement.forEach((topoentry) => {
+        if (topoentry.isIntersecting) {
+            topoentry.target.classList.add('topoanimate') + topoentry.target.classList.remove('topo')
+        } else {
+            topoentry.target.classList.remove('topoanimate')
+        }
+    })
+})
+
+const elementtopo = document.querySelectorAll('.topo')
+elementtopo.forEach((elementstopo) => myobservertopo.observe(elementstopo))
