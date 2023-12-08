ad4c0d3cc081970e97d5a2bcf0b1297847e2c190
GustavoSalamy <iphonegustavosalamy@outlook.com>
2023-12-07 23:47:21 -0300

    adicionando efeitos

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//animação divs dicas
 const myobserver = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
         if (entry.isIntersecting) {
@@ -9,24 +10,23 @@ const myobserver = new IntersectionObserver((entries) => {
 })
 
 const elements = document.querySelectorAll('#dicas')
-
 elements.forEach((element) => myobserver.observe(element))
+//animação divs dicas
 
-
-
+//animação h2 tema
 const myobserverh2 = new IntersectionObserver((h2element) => {
     h2element.forEach((h2entry) => {
         if (h2entry.isIntersecting) {
-            h2entry.target.classList.add('quemsoueuanimate') + h2entry.target.classList.remove('quemsoueu')
+            h2entry.target.classList.add('h2temaanimate') + h2entry.target.classList.remove('h2tema')
         }
     })
 })
 
-const elementh2 = document.querySelectorAll('.quemsoueu')
+const elementh2 = document.querySelectorAll('.h2tema')
 elementh2.forEach((elementsh2) => myobserverh2.observe(elementsh2))
+//animação h2 tema
 
-
-
+//animação cabeça
 const myobservertopo = new IntersectionObserver((topoelement) => {
     topoelement.forEach((topoentry) => {
         if (topoentry.isIntersecting) {
@@ -39,8 +39,9 @@ const myobservertopo = new IntersectionObserver((topoelement) => {
 
 const elementtopo = document.querySelectorAll('.topo')
 elementtopo.forEach((elementstopo) => myobservertopo.observe(elementstopo))
+//animação cabeça
 
-
+//animação foto quem sou eu
 const myobserverfoto = new IntersectionObserver((fotoelement) => {
     fotoelement.forEach((fotoentry) => {
         if (fotoentry.isIntersecting === true || window.scrollY > 10) {
@@ -51,3 +52,30 @@ const myobserverfoto = new IntersectionObserver((fotoelement) => {
 
 const elementfoto = document.querySelectorAll('.fotoquemsoueu')
 elementfoto.forEach((elementsfoto) => myobserverfoto.observe(elementsfoto))
+//animação foto que sou eu
+
+//animação <p> quem sou eu
+const myobserverpqseu = new IntersectionObserver((pqseuelement) => {
+    pqseuelement.forEach((pqseuentry) => {
+        if (pqseuentry.isIntersecting) {
+            pqseuentry.target.classList.add('pquemsoueuanimate') + pqseuentry.target.classList.remove('pquemsoueu')
+        }
+    })
+})
+
+const elementpqseu = document.querySelectorAll('.pquemsoueu')
+elementpqseu.forEach((elementspqseu) => myobserverpqseu.observe(elementspqseu))
+//animação <p> quem sou eu
+
+//animação NAVs receitas
+const myobservernavreceita = new IntersectionObserver((navreceitaelement) => {
+    navreceitaelement.forEach((navreceitaentry) => {
+        if (navreceitaentry.isIntersecting) {
+            navreceitaentry.target.classList.add('receitaanimation') + navreceitaentry.target.classList.remove('receita')
+        }
+    })
+})
+
+const elementnavreceita = document.querySelectorAll('.receita')
+elementnavreceita.forEach((elementsnavreceita) => myobservernavreceita.observe(elementsnavreceita))
+//animação NAVs receitas
